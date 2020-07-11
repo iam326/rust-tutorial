fa722cf98e18ca2a05383204f335feb74e32fcd9
t.takahashi <takahashi.developer@gmail.com>
2020-07-11 17:30:42 +0900

    予想値と比較する

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -13,7 +14,18 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Too smaill!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal => println!("You win!"),
+    }
 }
